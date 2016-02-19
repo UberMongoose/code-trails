2644c3ed7663fcd2fce8557b2ed1afebb642d755
UberMongoose <mongoose7@gmail.com>
2016-02-19 08:50:32 +0530

    C# Properties 1;

---
+++ C#/LanguageGeneral/LanguageGeneral/Properties.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LanguageGeneral
+{
+    internal class Customer
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+    }
+
+    [TestClass]
+    public class Properties
+    {
+        [TestMethod]
+        public void TestProperties()
+        {            
+            Customer customer = new Customer();
+            customer.Number = 9;
+            customer.Name = "Mrs Lovett's Pie Shop";
+        }
+    }
+}
